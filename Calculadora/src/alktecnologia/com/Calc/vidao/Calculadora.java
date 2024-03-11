dcf915c9a0273c83e685302742cbac9d7d110d19
Andre Luis Kunert <58589369+andreluiskunert@users.noreply.github.com>
2024-03-10 22:17:10 -0300

    Add files via upload

---
+++ Calculadora/src/alktecnologia/com/Calc/vidao/Calculadora.java
@@ -0,0 +1,19 @@
+package alktecnologia.com.Calc.vidao;
+
+import javax.swing.JFrame;
+	@SuppressWarnings("serial")
+	public class Calculadora extends JFrame {
+		public Calculadora() {
+			setSize(232,  322);
+			setDefaultCloseOperation(EXIT_ON_CLOSE);
+			setLocationRelativeTo(null);
+			setVisible(true);
+		}
+	  
+	     public void main(String[] args) {
+	    	    new Calculadora() ;
+	     }
+
+	}
+
+
